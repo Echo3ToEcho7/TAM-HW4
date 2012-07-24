175a339d2955f78a83e404aeaf16445826b57430
Colin O'Brien <cobrien@rallydev.com>
2012-07-24 17:39:21 -0400

    Added FindAll

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ TAM-HW4
 =======
 
 Week 4's homework
+
+RallyConnection API
+===================
+
+** new RallyConnection(opts) **
+
+*** Possible Options ***
+
+ * server - The server to connect to. [default: "rally1.rallydev.com"]
+ * username - The username to connect with.
+ * password - The password to use to connect with.
+ * version - The WSAPI version to use. [default: "1.36"]
+
+*** Example ***
+
+	var connection = new RallyConnection({
+		username: "paul@acme.com",
+		password: "SuperSecret!"
+	});
+
+
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var rally = require("./lib/rallyapi");
 
+// Create a new connection to Rally
 var conn = new rally.RallyConnection({
     username: "cobrien@rallydev.com",
     password: "Just4Rally",
@@ -8,28 +9,37 @@ var conn = new rally.RallyConnection({
 
 console.log("Connecting to Rally");
 
-conn.connect().when(function connected(results) {
-   console.log("We are connected"); 
-	var now = new Date();
+// The connect method will validate the connection config
+// and will give you your user object
+conn.connect().when(function connected(user) {
+	console.log("We are connected"); 
 
-   conn.find({
+	// We want to find all the TimeEntryItems
+	conn.findAll({
 	   type: "TimeEntryItem",
-	   fetch: ["Task", "Actuals", "Name", "Values", "Hours"].join(","),
-	   query: '(Task.Iteration.State = "Committed")'
+	   fetch: ["Task", "Actuals", "Name", "Values", "Hours"].join(",")
 	}).when(function foundTasks(results) {
+		// When done, we will be processing the results
 		var res = results.QueryResult.Results;
 
+		// This function will handle the processing of the results
+		// A for loop will not work due to the asynchronous nature
+		// of JavaScript
 		var processNext = function processNext(resList) {
+			// If there is nothing in the list, we are done
 			if (resList.length === 0) {
 				return;
 			}
 
+			// Array.shift will pull the first item off the array
 			var current = resList.shift();
 			var j, jj;
 
+			// Will will need to calculate the Totals
 			var total = 0;
 			console.log(current.Task.Name, current.Task.Actuals, current.Values.length);
 
+			// Loop through all the TimeEntryValues gathering the Hours
 			for (j = 0, jj = current.Values.length; j < jj; j++) {
 				console.log("\t", current.Values[j].Hours);
 				total += parseInt("" + current.Values[j].Hours, 10);
@@ -37,15 +47,18 @@ conn.connect().when(function connected(results) {
 
 			console.log("\tTotal", total);
 
+			// Update the Task with the Actuals computed from the Time Sheet
 			conn.update(current.Task._ref, {
 				Task: {
 					Actuals: total
 				}
 			}).when(function (taskUpdate) {
+				// When done, process the next item on the list
 				processNext(resList);
 			});
 		};	
 
+		// Kick off the process
 		processNext(res);
    });
 });
